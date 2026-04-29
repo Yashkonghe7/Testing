3140478c17791ef5efc205e970ec3ea47d646b25
Yash Konghe <91724530+Yashkonghe7@users.noreply.github.com>
2026-04-29 12:04:25 +0530

    Add Selenium test for Swag Labs page title

---
+++ test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from time import sleep
+
+
+# Set options for not prompting DevTools information
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+print("testing started")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.saucedemo.com/")
+sleep(3)
+
+# Get page title
+title = driver.title
+
+# Test if title is correct
+assert "Swag Labs" in title
+print("TEST 0: `title` test passed")
+
+# Close the driver
+driver.quit()
